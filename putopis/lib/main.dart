5b88eb4cf2758f7951d85f828d1c6dc4d05759cd
Salih Agić <salih.agic.dev@gmail.com>
2025-03-12 22:20:53 +0100

    ADD: Firebase set up to load cities

--- putopis/lib/main.dart
+++ putopis/lib/main.dart
@@ -1,7 +1,43 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FirebaseDatabase database = FirebaseDatabase.instance;
+
+  DatabaseReference ref = database.ref("cities");
+  final snapshot = await ref.get();
+  final data = (snapshot.value as dynamic);
+
+  final List<City> cities = data.entries
+      .map<City>(
+        (x) => City(
+          name: (x.value as dynamic)['name'],
+        ),
+      )
+      .toList();
+
+  for (final city in cities) {
+    print(city.name);
+  }
+
   runApp(const MainApp());
+}
+
+class City {
+  final String name;
+
+  City({
+    required this.name,
+  });
 }
 
 class MainApp extends StatelessWidget {
@@ -11,6 +47,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.red,
         body: Center(
           child: Text(
             'Hello World!',

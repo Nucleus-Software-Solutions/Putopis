be381b9b48fb0b7859f63629a0d983fac809798d
Salih Agić <salih.agic.dev@gmail.com>
2025-03-13 22:52:21 +0100

    Todos added

--- putopis/lib/features/destinations/widgets/destination_card.dart
+++ putopis/lib/features/destinations/widgets/destination_card.dart
@@ -9,64 +9,71 @@ class DestinationCard extends StatelessWidget {
   Widget build(BuildContext context) {
     final height = 120.0;
 
-    return Container(
-      color: AppColors.secondarySurfaceColor,
-      padding: EdgeInsets.all(10),
-      height: height,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-            child: Image.network(
-              'https://media.istockphoto.com/id/1160947136/photo/couple-relax-on-the-beach-enjoy-beautiful-sea-on-the-tropical-island.jpg?s=612x612&w=0&k=20&c=WJWEH22TFinjI0edzblfH-Nw0cdBfPX5LV8EMvs8Quo=',
-              fit: BoxFit.cover,
-              height: height,
-              width: height * 1.1,
+    return InkWell(
+      onTap: () {
+        // TODO: Push details page
+      },
+      child: Container(
+        color: AppColors.secondarySurfaceColor,
+        padding: EdgeInsets.all(10),
+        height: height,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              child: Image.network(
+                'https://media.istockphoto.com/id/1160947136/photo/couple-relax-on-the-beach-enjoy-beautiful-sea-on-the-tropical-island.jpg?s=612x612&w=0&k=20&c=WJWEH22TFinjI0edzblfH-Nw0cdBfPX5LV8EMvs8Quo=',
+                fit: BoxFit.cover,
+                height: height,
+                width: height * 1.1,
+              ),
             ),
-          ),
-          const SizedBox(width: 20),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Hawaii',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600,
-                    color: AppColors.primaryTextColor,
+            const SizedBox(width: 20),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Hawaii',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: AppColors.primaryTextColor,
+                    ),
                   ),
-                ),
-                Text(
-                  'Putovanje na Hawaii na 10 dana. Nemojte propustiti ovu avanturu!',
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                    color: AppColors.secondaryTextColor,
+                  Text(
+                    'Putovanje na Hawaii na 10 dana. Nemojte propustiti ovu avanturu!',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                      color: AppColors.secondaryTextColor,
+                    ),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
                   ),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                Text(
-                  '5600KM',
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w600,
-                    color: AppColors.primaryTextColor,
+                  Text(
+                    '5600KM',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                      color: AppColors.primaryTextColor,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.bookmark_border,
+            IconButton(
+              onPressed: () {
+                // TODO: Toggle bookmark for current destination
+              },
+              icon: Icon(
+                Icons.bookmark_border,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }

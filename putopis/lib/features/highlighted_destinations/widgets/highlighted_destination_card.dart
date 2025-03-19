18a9e4af81f9ed01bd4f2cc3fd5bcd065322fbe0
Salih Agić <salih.agic.dev@gmail.com>
2025-03-20 00:02:18 +0100

    Highlighted card border radius updated

--- putopis/lib/features/highlighted_destinations/widgets/highlighted_destination_card.dart
+++ putopis/lib/features/highlighted_destinations/widgets/highlighted_destination_card.dart
@@ -8,6 +8,7 @@ class HighlightedDestinationCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      borderRadius: BorderRadius.circular(12),
       onTap: () {
         Navigator.of(context).push(
           MaterialPageRoute(
